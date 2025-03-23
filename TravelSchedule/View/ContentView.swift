9c06bf583742fd66bfd17f75bc9e14d47d977308
Maxoscaro <svgerasimov92@yandex.ru>
2025-03-23 13:45:45 +0300

    T

--- TravelSchedule/View/ContentView.swift
+++ TravelSchedule/View/ContentView.swift
@@ -24,7 +24,6 @@ struct ContentView: View {
     }
     
     var body: some View {
-        
         NavigationStack(path: $navigationModel.path) {
             TabView {
                 MainScreenView()
